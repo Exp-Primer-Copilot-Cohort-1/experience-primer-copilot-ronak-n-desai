f49a607b40f6523bb9732b53f2c5a84ec1611926
Ronak Desai <98538788+ronak-n-desai@users.noreply.github.com>
2023-09-28 19:59:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,109 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "level": 7
+        },
+        {
+            "name": "CSS",
+            "level": 6
+        },
+        {
+            "name": "JavaScript",
+            "level": 7
+        },
+        {
+            "name": "PHP",
+            "level": 5
+        },
+        {
+            "name": "MySQL",
+            "level": 3
+        },
+        {
+            "name": "C",
+            "level": 3
+        },
+        {
+            "name": "C++",
+            "level": 3
+        },
+        {
+            "name": "Python",
+            "level": 3
+        },
+        {
+            "name": "Java",
+            "level": 3
+        },
+        {
+            "name": "Ruby",
+            "level": 3
+        },
+        {
+            "name": "Perl",
+            "level": 3
+        },
+        {
+            "name": "C#",
+            "level": 3
+        },
+        {
+            "name": "VB",
+            "level": 3
+        },
+        {
+            "name": "Swift",
+            "level": 3
+        },
+        {
+            "name": "Objective-C",
+            "level": 3
+        },
+        {
+            "name": "Assembly",
+            "level": 3
+        },
+        {
+            "name": "Go",
+            "level": 3
+        },
+        {
+            "name": "R",
+            "level": 3
+        },
+        {
+            "name": "MATLAB",
+            "level": 3
+        },
+        {
+            "name": "Kotlin",
+            "level": 3
+        },
+        {
+            "name": "Scala",
+            "level": 3
+        },
+        {
+            "name": "Lua",
+            "level": 3
+        },
+        {
+            "name": "Lisp",
+            "level": 3
+        },
+        {
+            "name": "Haskell",
+            "level": 3
+        },
+        {
+            "name": "F#",
+            "level": 3
+        },
+        {
+            "name": "Rust",
+            "level": 3
+        },
+        {
+            "name": "Erlang",
+            "level":
